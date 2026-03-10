5de418eee208fd924fafd23ba781604078ccf06a
nehaa1418 <nr3451@srmist.edu.in>
2026-03-10 08:54:43 +0530

    UC2:Display Hello with command-line argument

--- HelloApp.java
+++ HelloApp.java
@@ -1,9 +1,12 @@
-// UC1: Display Hello World
 public class HelloApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        if (args.length > 0) {
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
 
     }
 
